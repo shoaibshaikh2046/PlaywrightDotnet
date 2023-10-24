4cd10b6c7342bdd9edeac158f04e62d68525e0c2
shoaib.shaikh <shoaibshaikh2046@gmail.com>
2023-10-24 23:32:23 +0530

    Added Code Gen Test and Tracing Test

---
+++ PlaywrightDotnet/Tests/CodeGenTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace PlaywrightDotnet.Tests
+{
+    [TestFixture]
+    public class CodeGenTest
+    {
+
+        [Test]
+        public async Task TestCodeGen()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+            });
+            var context = await browser.NewContextAsync();
+
+            var page = await context.NewPageAsync();
+
+            await page.GotoAsync("https://demo.playwright.dev/todomvc/#/");
+
+            await page.GetByPlaceholder("What needs to be done?").ClickAsync();
+
+            await page.GetByPlaceholder("What needs to be done?").FillAsync("CodeGeneration");
+
+            await page.GetByPlaceholder("What needs to be done?").PressAsync("Enter");
+
+            await page.GetByPlaceholder("What needs to be done?").ClickAsync();
+
+            await page.GetByPlaceholder("What needs to be done?").FillAsync("Recording");
+
+            await page.GetByPlaceholder("What needs to be done?").PressAsync("Enter");
+
+            await page.GetByPlaceholder("What needs to be done?").ClickAsync();
+
+            await page.GetByPlaceholder("What needs to be done?").FillAsync("Playwright");
+
+            await page.GetByPlaceholder("What needs to be done?").PressAsync("Enter");
+
+            await page.Locator("li").Filter(new() { HasText = "CodeGeneration" }).GetByLabel("Toggle Todo").CheckAsync();
+
+            await page.GetByRole(AriaRole.Link, new() { Name = "Completed" }).ClickAsync();
+
+            await page.GetByRole(AriaRole.Link, new() { Name = "Active" }).ClickAsync();
+
+            await page.GetByRole(AriaRole.Link, new() { Name = "All" }).ClickAsync();
+        }
+    }
+}
---
+++ PlaywrightDotnet/Tests/TracingTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace PlaywrightDotnet.Tests
+{
+    [TestFixture]
+    public class TracingTest
+    {
+
+        [Test]
+        public async Task TestTracing()
+        {
+            var playwright = await Playwright.CreateAsync();
+
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+                Channel = "chrome"
+            });
+
+            // one browser context
+            var context = await browser.NewContextAsync();
+
+            await context.Tracing.StartAsync(new()
+            {
+                Screenshots = true,
+                Snapshots = true,
+                Sources = true,
+
+            });
+
+
+            // page 
+            var page = await context.NewPageAsync();
+
+            await page.GotoAsync("https://demo.playwright.dev/todomvc/");
+
+            List<string> expected = new List<string>() { "Shoaib", "Testing", "Playwright" };
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                await page.GetByPlaceholder("What needs to be done?").FillAsync(expected[i]);
+                await page.Keyboard.PressAsync("Enter");
+            }
+
+            await page.Locator("//label[.='" + expected.FirstOrDefault() + "']/parent::div/input").ClickAsync();
+
+
+            await context.Tracing.StopAsync(new()
+            {
+                Path = "traces/trace.zip"
+
+            });
+        }
+    }
+}
